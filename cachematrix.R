96b2b982e29550409aacff5e968f954b99bb28ac
Jason Yamada-Hanff <jyamada1@gmail.com>
2017-07-03 15:54:14 -0700

    add implementation of cached matrix for expensive matrix operations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cached/memoized matrix operations
+##
+## A cachematrix created by `makeCacheMatrix` can save results of
+## expensive matrix operations to avoid recomputation
+##
+## for R Programming course in JHU Data Science Coursera track
 
-## Write a short comment describing this function
 
+## create a cachematrix that can store its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set = function(x_) {
+    x <<- x_
+    inv <<- NULL
+  }
+  list(set = set,
+       get = function() x,
+       setinverse = function(inv_) inv <<- inv_,
+       getinverse = function() inv)
 }
 
-
-## Write a short comment describing this function
-
+## return matrix inverse of x, using cached result if available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if (!is.null(inv))
+    return(inv)
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
